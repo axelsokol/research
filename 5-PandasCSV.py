b4bea41b19efa8fca912dd9bece828d42f560b81
axelsokol <164379756+axelsokol@users.noreply.github.com>
2024-03-24 18:02:53 -0400

    Update 5-PandasCSV.py

--- 5-PandasCSV.py
+++ 5-PandasCSV.py
@@ -3,20 +3,12 @@ import pandas as pd
 
 df = pd.read_csv("C:\\Users\\osokolov\\Desktop\\Python Mix\\cerial_data\\cerial_data\\cereal.csv" )
 
-
-
 print(df.describe())
-
 print(df.head(10 ))
 print(df[["Cereal Name","Calories"]])
-
 print(df[5:12][1:5])
-
-
 #df.iloc [row index range, column index range]
-
 print(df.iloc[1:5, 0:5])
-
 print("\n---- df.loc\n", df.loc[[1, 2, 3, 10, 12],['Cereal Name', 'Manufacturer']])
 
 # https://www.youngwonks.com/blog/top-10-ways-to-filter-pandas-dataframe
